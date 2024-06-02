129b1d3ca685b7ef972f09a03eabe8c7e7aebe79
Gedis24 <163135699+Gedis24@users.noreply.github.com>
2024-06-02 22:48:16 +0300

    Add files via upload

---
+++ Egzaminas/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.TextBox textBoxLoginPassword;
+        private System.Windows.Forms.Button buttonRegister;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.Button buttonBruteForce;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.textBoxLoginPassword = new System.Windows.Forms.TextBox();
+            this.buttonRegister = new System.Windows.Forms.Button();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.buttonBruteForce = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Location = new System.Drawing.Point(284, 115);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(336, 26);
+            this.textBoxUsername.TabIndex = 0;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(284, 163);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(336, 26);
+            this.textBoxPassword.TabIndex = 1;
+            // 
+            // textBoxLoginPassword
+            // 
+            this.textBoxLoginPassword.Location = new System.Drawing.Point(284, 215);
+            this.textBoxLoginPassword.Name = "textBoxLoginPassword";
+            this.textBoxLoginPassword.Size = new System.Drawing.Size(336, 26);
+            this.textBoxLoginPassword.TabIndex = 2;
+            // 
+            // buttonRegister
+            // 
+            this.buttonRegister.Location = new System.Drawing.Point(395, 269);
+            this.buttonRegister.Name = "buttonRegister";
+            this.buttonRegister.Size = new System.Drawing.Size(121, 37);
+            this.buttonRegister.TabIndex = 3;
+            this.buttonRegister.Text = "Registruotis";
+            this.buttonRegister.UseVisualStyleBackColor = true;
+            this.buttonRegister.Click += new System.EventHandler(this.buttonRegister_Click);
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(522, 269);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(121, 37);
+            this.buttonLogin.TabIndex = 4;
+            this.buttonLogin.Text = "Prisijungti";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // buttonBruteForce
+            // 
+            this.buttonBruteForce.Location = new System.Drawing.Point(284, 269);
+            this.buttonBruteForce.Name = "buttonBruteForce";
+            this.buttonBruteForce.Size = new System.Drawing.Size(105, 37);
+            this.buttonBruteForce.TabIndex = 5;
+            this.buttonBruteForce.Text = "Brute Force";
+            this.buttonBruteForce.UseVisualStyleBackColor = true;
+            this.buttonBruteForce.Click += new System.EventHandler(this.buttonBruteForce_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(328, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(221, 37);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Prisijungimas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(165, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Vartotojas";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(69, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(209, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Registracijos slaptažodis";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(80, 215);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(201, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Prisijungimo slaptažodis";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(801, 376);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxUsername);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxLoginPassword);
+            this.Controls.Add(this.buttonRegister);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.buttonBruteForce);
+            this.Name = "Form1";
+            this.Text = "Prisijungimo sistema";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ Egzaminas/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonRegister_Click(object sender, EventArgs e)
+        {
+            string username = textBoxUsername.Text;
+            string password = textBoxPassword.Text;
+            string hashedPassword = PasswordHelper.HashPassword(password);
+            UserStore.AddUser(username, hashedPassword);
+            MessageBox.Show("User registered successfully!");
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            string username = textBoxUsername.Text;
+            string password = textBoxLoginPassword.Text;
+            bool isAuthenticated = Authentication.ValidateLogin(username, password);
+
+            MessageBox.Show(isAuthenticated ? "Login successful!" : "Login failed!");
+        } 
+        public static class Authentication
+        {
+            public static bool ValidateLogin(string username, string password)
+            {
+                string storedHashedPassword = UserStore.GetHashedPassword(username);
+
+                if (storedHashedPassword == null)
+                {
+                    return false; // User not found
+                }
+
+                string hashedInputPassword = PasswordHelper.HashPassword(password);
+
+                return storedHashedPassword == hashedInputPassword;
+            }
+        }
+        public static class PasswordHelper
+        {
+            private static readonly string FixedSalt = "Ykdcr";
+
+            public static string HashPassword(string password)
+            {
+                using (SHA256 sha256 = SHA256.Create())
+                {
+                    byte[] saltedPassword = Encoding.UTF8.GetBytes(FixedSalt + password);
+                    byte[] hashBytes = sha256.ComputeHash(saltedPassword);
+                    return Convert.ToBase64String(hashBytes);
+                }
+            }
+        }
+        public static class UserStore
+        {
+            private static Dictionary<string, string> users = new Dictionary<string, string>();
+
+            public static void AddUser(string username, string hashedPassword)
+            {
+                users[username] = hashedPassword;
+            }
+
+            public static string GetHashedPassword(string username)
+            {
+                return users.ContainsKey(username) ? users[username] : null;
+            }
+        }
+
+        
+        private async void buttonBruteForce_Click(object sender, EventArgs e)
+        {
+            string username = textBoxUsername.Text;
+            string storedHashedPassword = UserStore.GetHashedPassword(username);
+            if (storedHashedPassword != null)
+            {
+                string crackedPassword = await Task.Run(() => BruteForceAttack.PerformBruteForce(storedHashedPassword, 4, 4)); // maxLength and numThreads can be adjusted
+                MessageBox.Show(crackedPassword != null ? $"Password cracked: {crackedPassword}" : "Password not found.");
+            }
+            else
+            {
+                MessageBox.Show("User not found.");
+            }
+        }
+        public static class BruteForceAttack
+        {
+            public static string PerformBruteForce(string hashedPassword, int maxLength, int numThreads)
+            {
+                string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+                ConcurrentQueue<string> queue = new ConcurrentQueue<string>();
+
+                List<Task<string>> tasks = new List<Task<string>>();
+
+                for (int i = 0; i < numThreads; i++)
+                {
+                    tasks.Add(Task.Run(() => BruteForceWorker(hashedPassword, chars, queue, maxLength)));
+                }
+
+                // Seed the queue with initial values
+                foreach (char c in chars)
+                {
+                    queue.Enqueue(c.ToString());
+                }
+
+                Task.WaitAll(tasks.ToArray());
+
+                foreach (var task in tasks)
+                {
+                    if (task.Result != null)
+                    {
+                        return task.Result;
+                    }
+                }
+
+                return null;
+            }
+
+            private static string BruteForceWorker(string hashedPassword, string chars, ConcurrentQueue<string> queue, int maxLength)
+            {
+                while (queue.TryDequeue(out string current))
+                {
+                    if (current.Length >= maxLength)
+                    {
+                        continue;
+                    }
+
+                    string currentHashedPassword = PasswordHelper.HashPassword(current);
+
+                    if (currentHashedPassword == hashedPassword)
+                    {
+                        return current;
+                    }
+
+                    foreach (char c in chars)
+                    {
+                        queue.Enqueue(current + c);
+                    }
+                }
+                return null;
+            }
+        }
+
+        
+    }
+}
